61cadae8437622e5dbf7d8667880a3eb9c6eeb78
Slayred <aleksch1989@gmail.com>
2021-09-16 17:58:28 +0300

    add some test for ssl

--- app/src/test/java/com/example/joke_lesson_8/ExampleUnitTest.kt
+++ app/src/test/java/com/example/joke_lesson_8/ExampleUnitTest.kt
@@ -3,6 +3,13 @@ package com.example.joke_lesson_8
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.BufferedInputStream
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.UnknownHostException
+import javax.net.ssl.SSLHandshakeException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,6 +19,20 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val joke = "https://yesno.wtf/api"
+        var connection: HttpURLConnection? = null
+        try {
+            val url = URL(joke)
+            connection = url.openConnection() as HttpURLConnection?
+            InputStreamReader(BufferedInputStream(connection?.inputStream)).use{
+                println(it.readText())
+            }
+        }catch (e: Exception){
+            if (e is SSLHandshakeException)
+                println("SSL")
+            if (e is UnknownHostException)
+                println("HOST")
+            else println("ERROR")
+        }
     }
 }
