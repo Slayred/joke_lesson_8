1699ab5f904a408ddb7cec9dc9d1ee18ac4db105
Slayred <aleksch1989@gmail.com>
2021-10-18 21:58:13 +0300

    12 4

--- app/src/main/java/com/example/joke_lesson_8/nonuse/TestCloudDataSource.kt
+++ app/src/main/java/com/example/joke_lesson_8/nonuse/TestCloudDataSource.kt
@@ -1,8 +1,9 @@
-package com.example.joke_lesson_8.model
+package com.example.joke_lesson_8.nonuse
 
 import com.example.joke_lesson_8.data.Result
 import com.example.joke_lesson_8.interfaces.CloudDataSource
 import com.example.joke_lesson_8.data.ErrorType
+import com.example.joke_lesson_8.model.JokeServerModel
 
 class TestCloudDataSource: CloudDataSource {
 //    private var count = 0
