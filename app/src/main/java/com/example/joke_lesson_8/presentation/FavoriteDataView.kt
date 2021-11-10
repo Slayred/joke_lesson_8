725cbcdd3d19f7d3f353277fe611491957f99954
Slayred <aleksch1989@gmail.com>
2021-11-10 13:12:48 +0300

    17 - 2

--- app/src/main/java/com/example/joke_lesson_8/presentation/FavoriteDataView.kt
+++ app/src/main/java/com/example/joke_lesson_8/presentation/FavoriteDataView.kt
@@ -2,12 +2,38 @@ package com.example.joke_lesson_8.presentation
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import com.example.joke_lesson_8.R
 
-class FavoriteDataView: View {
+class FavoriteDataView: LinearLayout {
+
+    private val checkBox: CheckBox
+    private val textView: CorrectTextView
+    private val changeButton: CorrectImageButton
+    private val actionButton: CorrectButton
+    private val progressBar: CorrectProgressBar
 
     constructor(context: Context): super(context)
     constructor(context: Context, attrs: AttributeSet): super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+    init {
+        orientation = VERTICAL
+        (context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+        .inflate(R.layout.favorite_data_view,this,true)
+
+        val linear = getChildAt(1) as LinearLayout
+        checkBox = linear.findViewById(R.id.checkBox)
+        textView = linear.findViewById(R.id.text)
+        changeButton = linear.findViewById(R.id.changeBtn)
+        progressBar = linear.findViewById(R.id.progressBar)
+        actionButton = linear.findViewById(R.id.btnJoke)
+
+    }
 
 }
